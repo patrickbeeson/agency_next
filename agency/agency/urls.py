d125fdfe47dfdd64774156419a3ffbaaf9fc6a53
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-23 11:11:50 -0400

    Create homepage module and write passing test for homepage view.

--- agency/agency/urls.py
+++ agency/agency/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'homepage.views.home_page', name='home')
     # url(r'^$', 'agency.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
