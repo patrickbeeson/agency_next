eb45e34e74e3e64ef966253c0ae9e5bdec735a12
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-28 14:55:10 -0400

    Add settings for various environments.

--- agency/agency/urls.py
+++ agency/agency/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns(
     '',
@@ -7,5 +9,13 @@ urlpatterns = patterns(
     # url(r'^$', 'agency.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
