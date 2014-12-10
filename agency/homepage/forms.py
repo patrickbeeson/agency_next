91a090c33b60c51ab9a5a5b35c26688ba8681031
Patrick Beeson <patrickbeeson@gmail.com>
2014-12-10 15:07:49 -0500

    Add widget attributes to form fields, change required flag, add form to home template.

--- agency/homepage/forms.py
+++ agency/homepage/forms.py
@@ -19,14 +19,28 @@ class AgencyContactForm(ContactForm):
     interest = forms.ChoiceField(
         choices=INTEREST_CHOICES,
         label=u'What are you interested in?',
-        required=False,
+        required=True,
+        widget=forms.Select(
+            attrs={
+                'class': 'form-select',
+                'data-parsley-trigger': 'change',
+                'data-parsley-error-message': 'Knowing your subject is very important to us.',
+            }
+        )
     )
     phone = forms.RegexField(
         regex=r'^\d{3}[\-]\d{3}[\-]\d{4}([\s][a-z]{3}[\s]\d{1,5})?$',
         help_text='Required format: 555-555-5555 or 555-555-5555 ext 12345',
         required=False,
         label=u'Phone',
-        error_message=('Phone number must match the required format!')
+        error_message=('Phone number must match the required format!'),
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'data-parsley-trigger': 'change',
+                'data-parsley-error-message': 'Oops! Is this a valid phone number?',
+            }
+        )
     )
 
     def __init__(self, *args, **kwargs):
@@ -34,6 +48,33 @@ class AgencyContactForm(ContactForm):
         Change the ordering of fields.
         """
         super(AgencyContactForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget = forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'data-parsley-trigger': 'change',
+                'data-parsley-error-message': 'We need your name',
+                'required': 'required',
+            }
+        )
+        self.fields['name'].required = True
+        self.fields['email'].widget = forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'data-parsley-trigger': 'change',
+                'data-parsley-error-message': 'We need a valid email address',
+                'required': 'required',
+            }
+        )
+        self.fields['email'].required = True
+        self.fields['body'].widget = forms.Textarea(
+            attrs={
+                'class': 'form-textarea',
+                'data-parsley-trigger': 'change',
+                'data-parsley-error-message': 'Looks like you forgot to include your message.',
+                'required': 'required',
+            }
+        )
+        self.fields['body'].required = True
         original_fields = self.fields
         new_ordering = OrderedDict()
         for key in ['name', 'email', 'phone', 'interest', 'body']:
