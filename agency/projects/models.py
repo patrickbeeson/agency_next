04c3c59f2258c1fa4d462276d1294bf3e8a09c1b
Patrick Beeson <patrickbeeson@gmail.com>
2014-12-16 15:43:24 -0500

    Add markup to assetgroup templates. Update choices in model.

--- agency/projects/models.py
+++ agency/projects/models.py
@@ -106,7 +106,7 @@ class AssetGroup(OrderedModel):
     TYPE = Choices(
         ('fullbleed', _('Full bleed')),
         ('centered', _('Centered')),
-        ('centerednarrow', _('Centered Narrow')),
+        ('narrow', _('Centered Narrow')),
         ('5050', _('50/50')),
         ('6030', _('60/30')),
         ('3060', _('30/60'))
